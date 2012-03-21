569704c476e8f5211de858d5ec174ecffee11908
josvazg (Jose Luis Vazquez González) <josvazg@gmail.com>
2012-03-21 19:50:07 +0100

    Problem 3 also in Go

--- pe3/pe3.go
+++ pe3/pe3.go
@@ -3,27 +3,30 @@ package main
 import (
 	"fmt"
 	"math"
+    "time"
 )
 
 type long int64;
 
 func maxFactor(target long) long {
-	factor:=1;
+	factor:=long(1);
     if((target%2)==0) {
     	factor=2;
     	for ;(target%2)==0; {
+            //fmt.Println("*factor=",factor)
     		target=target/2;
     	}
     }
-    i:=3;
-    topper:=math.Sqrt(target);
+    i:=long(3);
+    topper:=long(math.Sqrt(float64(target)));
     for ;target>1 && factor<=topper; {
     	if((target%i)==0) {
     		factor=i;
     		for ;(target%i)==0; {
     			target=target/factor;
+                //fmt.Println("factor=",factor)
     		}
-    		topper=math.Sqrt(target);
+    		topper=long(math.Sqrt(float64(target)));
     	}
     	i+=2;
     }	
@@ -34,5 +37,11 @@ func maxFactor(target long) long {
 }
 
 func main() {
-	fmt.Println("maxFactor(500)=",maxFactor())
+    t:=time.Now()
+	fmt.Println("maxFactor(500)=",maxFactor(500),"in",(time.Since(t)))
+    fmt.Println("maxFactor(600851475143)=",maxFactor(600851475143),"in",(time.Since(t)))
+    fmt.Println("maxFactor(600851475140)=",maxFactor(600851475140),"in",(time.Since(t)))
+    fmt.Println("maxFactor(60085147514)=",maxFactor(60085147514),"in",(time.Since(t)))
 }
+
+
