ed5113abcf29a78a05482713ccdcd6c76ba1a6d7
Jose Luis Vázquez González <josvazg@gmail.com>
2012-03-22 10:45:51 +0100

    Optimized O(1) solution added to Problem 6

--- pe6/pe6.go
+++ pe6/pe6.go
@@ -17,11 +17,22 @@ func difSqSum(max long) long {
 	return sqsum - sumsq
 }
 
+func difSqSumO1(max long) long {
+	sum := (max * (max + 1)) / 2
+	sumsq := (2*max + 1) * (max + 1) * max / 6
+	return (sum * sum) - sumsq
+}
+
 func main() {
 	fmt.Println("Project Eurler's Problem 6...")
 	fmt.Println("Difference between the sum of the squares and the square of the sums...")
 	t := time.Now()
 	fmt.Println("1-10:", difSqSum(10), "in", (time.Since(t)))
+	t = time.Now()
+	fmt.Println("O(1) 1-10:", difSqSumO1(10), "in", (time.Since(t)))
+	t = time.Now()
 	fmt.Println("1-100:", difSqSum(100), "in", (time.Since(t)))
+	t = time.Now()
+	fmt.Println("O(1) 1-100:", difSqSumO1(100), "in", (time.Since(t)))
 }
 
