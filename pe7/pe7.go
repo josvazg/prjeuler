5971a4fe67b5e691c7c6fc7f68012b97d89dbc30
josvazg (Jose Luis Vazquez González) <josvazg@gmail.com>
2012-03-24 23:10:15 +0100

    Primes are found hundreds of times faster with a few tricks from Project Eulers solution to Problem 7

--- pe7/pe7.go
+++ pe7/pe7.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"time"
 )
 
@@ -16,7 +17,7 @@ func isPrime(primes []long, a long) bool {
 	return true
 }
 
-func primes(max int) long {
+func primeAt(max int) long {
 	primes:=[]long{1,2,3}
 	for i := long(5); len(primes)<=max; i+=2 {
 		if isPrime(primes, i) {
@@ -27,12 +28,57 @@ func primes(max int) long {
 	return primes[len(primes)-1]
 }
 
+func isPrimePE(n int) bool {
+	if n==1 {
+		return false
+	} else if n<4 {
+		return true  //2 and 3 are prime
+	} else if n % 2==0 {
+		return false
+	} else if n<9 {
+		return true    //we have already excluded 4,6 and 8.
+	} else if n % 3==0 {
+		return false
+	} else {
+		r:=int(math.Floor( math.Sqrt(float64(n)) ))  // n rounded to the greatest integer r so that r*r<=n
+		f:=5
+		for f<=r {
+			if (n % f)==0 {
+				return false
+			}
+			if (n %(f+2))==0 {
+				return false
+			}
+			f=f+6
+		}
+		return true
+	}
+	return false
+}
+
+func primeAtPE(n int) int {
+	limit:=10001
+	count:=1 //we  know that 2 is prime
+	candidate:=3
+	for ;count<limit;candidate=candidate+2 {
+		if isPrimePE(candidate) { 
+			count=count+1
+			//fmt.Println(candidate,"is primer number",count)
+		}
+		if count==limit {
+			return candidate
+		}
+	}
+	return -1
+}
+
 func main() {
 	fmt.Println("Project Eurler's Problem 7...")
 	fmt.Println("10001st prime")
 
 	t := time.Now()
-	fmt.Println("10001st prime:", primes(10001), "in", (time.Since(t)))
-
+	fmt.Println("10001st prime:", primeAt(10001), "in", (time.Since(t)))
+	t = time.Now()
+	fmt.Println("10001st prime:", primeAtPE(10001), "in", (time.Since(t)))
 }
 
